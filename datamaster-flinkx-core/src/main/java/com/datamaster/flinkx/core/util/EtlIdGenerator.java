bf4e10695c1438bff89a3b8bd2709f1d0dc1f5eb
shanglili <lili.shang@wenge.com>
2026-06-15 23:08:01 +0800

    删除无用代码

--- datamaster-flinkx-core/src/main/java/com/datamaster/flinkx/core/util/EtlIdGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.datamaster.flinkx.core.util;
-
-import cn.hutool.core.util.IdUtil;
-
-public class EtlIdGenerator {
-    public static Long getLongId() {
-        return IdUtil.getSnowflakeNextId();
-    }
-}
